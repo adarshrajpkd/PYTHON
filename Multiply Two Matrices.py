ff2e9e7379c19670799bd07460d0230735618499
adarshrajpkd <60054806+adarshrajpkd@users.noreply.github.com>
2020-01-19 18:28:31 +0530

    Create Multiply Two Matrices.py

---
+++ Multiply Two Matrices.py
@@ -0,0 +1,7 @@
+ iterate through rows of X  
+for i in range(len(X)):  
+   for j in range(len(Y[0])):  
+       for k in range(len(Y)):  
+           result[i][j] += X[i][k] * Y[k][j]  
+for r in result:  
+   print(r)  
